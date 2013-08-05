fc2924ba30c01d3e8a68c44d64e2f637bde05454
Pavel V. Talanov <talanov.pavel@gmail.com>
2013-08-05 22:18:23 +0400

    Get rid of JetNodeTypes#LOOP_PARAMETER

--- idea/src/org/jetbrains/jet/plugin/intentions/SpecifyTypeExplicitlyAction.java
+++ idea/src/org/jetbrains/jet/plugin/intentions/SpecifyTypeExplicitlyAction.java
@@ -122,7 +122,7 @@ public class SpecifyTypeExplicitlyAction extends PsiElementBaseIntentionAction {
                  && !((JetNamedFunction) declaration).hasBlockBody()) {
             setText(JetBundle.message("specify.type.explicitly.add.return.type.action.name"));
         }
-        else if (declaration instanceof JetParameter && JetNodeTypes.LOOP_PARAMETER == declaration.getNode().getElementType()) {
+        else if (declaration instanceof JetParameter && declaration.getParent() instanceof JetForExpression) {
             if (((JetParameter) declaration).getTypeReference() != null) {
                 setText(JetBundle.message("specify.type.explicitly.remove.action.name"));
                 return true;
